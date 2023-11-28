2f2f457b58cdc9e515c27048cbccf10d7205cd01
Scott H. Hawley <scott.hawley@belmont.edu>
2023-11-28 10:37:01 -0600

    removed filler comments

--- setup.py
+++ setup.py
@@ -1,9 +1,7 @@
 from setuptools import setup, find_packages
 
-# Read the contents of your README file
 with open('README.md', 'r', encoding='utf-8') as fh:
     long_description = fh.read()
-
 
 setup(
     name='midi-player',
@@ -11,11 +9,9 @@ setup(
     url='https://github.com/drscotthawley/midi-player',
     project_urls={
         'Source': 'https://github.com/drscotthawley/midi-player',  
-        #'Documentation': 'URL_FOR_DOCUMENTATION',  # Optional
     },
     packages=find_packages(),
-    # install_requires=[  ], # -- No required packages! 
-    # Add additional metadata about your package
+    # install_requires=[  ], # -- Note: No required packages! 
     author='Scott H. Hawley',
     author_email='scott.hawley@belmont.edu',
     description='Python launcher of animated MIDI player by @cifkao & @magenta',
